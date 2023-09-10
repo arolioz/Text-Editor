ba94b31ce3e562f7bc4a1e69d58e71fcc8032879
Aaron <arolio301911@gmail.com>
2023-09-10 09:54:25 -0400

    Adding some comments

--- Menubar/MenubarCommands.py
+++ Menubar/MenubarCommands.py
@@ -5,8 +5,12 @@ from tkinter import filedialog
 def open():
     f = filedialog.askopenfile(initialdir="Files")
     fileName = f.name
+    
+    #Gets the name of the file
     file_path_components = fileName.split('/')
     file_name_and_extension = file_path_components[-1].rsplit('.', 1)
+
+    #Creates a tab and write the content from the selected file
     tab(file_name_and_extension[0],noteBook)
     t = tabs.get(file_name_and_extension[0])
     t.insert(1.0,f.read())
